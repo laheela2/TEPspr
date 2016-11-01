7abb07d73417f4d2b6cb0a2168105569feb18957
Jinu <jinuyoo@gmail.com>
2016-11-01 18:17:06 +0900

    board 이식 진행중

--- src/main/java/com/tep/board/controller/BoardController.java
+++ src/main/java/com/tep/board/controller/BoardController.java
@@ -11,11 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tep.board.service.BoardService;
+import com.tep.commons.common.PagingCalculator;
 
 @Controller
 public class BoardController {
 	Logger log = Logger.getLogger(this.getClass());
 
+	private static final int SEARCHKEY_SUBEJCT = 0;
+	private static final int SEARCHKEY_SUBJECT_CONTENT = 1;
+	private static final int SEARCHKEY_NAME = 2;
+	
+	private int currentPage = 1;
+	private int totalCount;
+	private int blockCount = 9;
+	private int blockPage = 3;
+	private String pagingHtml;
+	private PagingCalculator page;
+	
+	private int searchKey = -1;
+	private String searchWord = "";	
+	
 	@Resource(name = "boardService")
 	private BoardService boardService;
 	
@@ -24,8 +39,25 @@ public class BoardController {
         ModelAndView mv = new ModelAndView("board");
 
         List<Map<String,Object>> list = boardService.selectBoardList(map);
+        totalCount = list.size();
+		page = new PagingCalculator("board", currentPage, totalCount, blockCount, blockPage);
+		pagingHtml = page.getPagingHtml().toString();
+		
+		int lastCount = totalCount;
+		
+		if(page.getEndCount() < totalCount){
+			lastCount = page.getEndCount()+1;
+		}
+		
+		list = list.subList(page.getStartCount(), lastCount);
+        
+        
+        
         mv.addObject("list", list);
+        mv.addObject("pagingHtml",pagingHtml);
         
         return mv;
     }
+    
+    
 }
