8aed1c8cc48b1ad1168f1742fc98820d6418d15a
Saeyoung <Saeyoung@USER-PC>
2016-11-14 21:11:10 +0900

    Lendplace 관련 파일 7개 프로토타입 작성 완료!

--- src/main/java/com/tep/lendplace/dao/LendplaceDAO.java
+++ src/main/java/com/tep/lendplace/dao/LendplaceDAO.java
@@ -1,5 +1,18 @@
 package com.tep.lendplace.dao;
 
-public class LendplaceDAO {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
+import com.tep.commons.dao.AbstractDAO;
+
+@Repository("lendplaceDAO")
+public class LendplaceDAO extends AbstractDAO {
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectLendplaceList(Map<String, Object> map) {
+		return (List<Map<String, Object>>)selectList("lendplace.selectLendplaceList",map);
+	}
+	
 }
